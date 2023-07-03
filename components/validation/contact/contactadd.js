e862238a70604c8eff2a4eea0e2791182bcd6d84
sahilHussain <sahil.hussain@travel2kashmir.com>
2023-07-03 15:00:29 +0530

    phone number validation updated

--- components/validation/contact/contactadd.js
+++ components/validation/contact/contactadd.js
@@ -1,78 +1,81 @@
 import { parsePhoneNumber } from "awesome-phonenumber";
 
-const validateContact = (data,props) =>{
-    var error={};
-     var flag=[]
-     var final_flag=true;
-    
-    if(data.contact_type==="" || data.contact_type===undefined)
-    {
+const validateContact = (data, props) => {
+    var error = {};
+    var flag = []
+    var final_flag = true;
+
+    if (data.contact_type === "" || data.contact_type === undefined) {
         flag.push(false)
-        error.contact_type="APP: The contact type is required"
+        error.contact_type = "APP: The contact type is required"
     }
 
-    if(data.contact_data==="" || data.contact_data===undefined)
-    {
+    if (data.contact_data === "" || data.contact_data === undefined) {
         flag.push(false)
-        error.contact_data="APP: The contact data is required."
+        error.contact_data = "APP: The contact data is required."
     }
 
-    if(data.contact_type !== "" && data.contact_type!== undefined){
-        if(data.contact_type === "website"){  
-    if((!data.contact_data?.match(/^(?:http(s)?:\/\/)?[\w.-]+(?:\.[\w\.-]+)+[\w\-\._~:/?#[\]@!\$&'\(\)\*\+,;=.]+$/))){
-        flag.push(false)
-        error.contact_data="APP: The website is invalid."
-    }
-}
-    }
-    if(data.contact_type !== "" && data.contact_type!== undefined){
-        if(data.contact_type === "email"){  
-    if((!data.contact_data?.match(/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/))){
-        flag.push(false)
-        error.contact_data="APP: The email is invalid."
-    }
-}
-    }
-
-    if(data.contact_type !== "" && data.contact_type !== undefined){
-    if(data.contact_type === "phone"){  
-    const pn = parsePhoneNumber(data.contact_data, {regionCode:props})
-    if (pn?.valid == false) {
-        flag.push(false)
-        error.contact_data = `APP: The phone number is invalid.`
-      }
-}
-    }
-   
-    if(data.contact_type !== "" && data.contact_type !== undefined){
-        if(data.contact_type === "tdd number"){  
-        if((!data?.contact_data?.match(/^([1-9]+[0-9]*)$/))){
-            flag.push(false)
-          error.contact_data="APP: The tdd number is invalid."
-        }
-    }
-        }
-     if(data.contact_type !== "" && data.contact_type !== undefined){
-            if(data.contact_type === "toll free number"){  
-            if((!data?.contact_data?.match(/^([1-9]+[0-9]*)$/))){
+    if (data.contact_type !== "" && data.contact_type !== undefined) {
+        if (data.contact_type === "website") {
+            if ((!data.contact_data?.match(/^(?:http(s)?:\/\/)?[\w.-]+(?:\.[\w\.-]+)+[\w\-\._~:/?#[\]@!\$&'\(\)\*\+,;=.]+$/))) {
                 flag.push(false)
-                error.contact_data="APP: The toll free number is invalid."
+                error.contact_data = "APP: The website is invalid."
             }
         }
-     }
-
-    
-    for (let value in flag) {
-     
-        if(flag[value]=== false)
-         {
-           final_flag = false;
-           break;
-         }
-         
-      } 
-
-      return final_flag===true ? true : error;
     }
-export default  validateContact
+    if (data.contact_type !== "" && data.contact_type !== undefined) {
+        if (data.contact_type === "email") {
+            if ((!data.contact_data?.match(/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/))) {
+                flag.push(false)
+                error.contact_data = "APP: The email is invalid."
+            }
+        }
+    }
+
+    if (data.contact_type !== "" && data.contact_type !== undefined) {
+        if (data.contact_type === "phone") {
+            // regex to check string may have + at start , may have - after 2 digits and must have only numbers
+            const regex = /^(\+)?\d{2}(-)?\d+$/;
+            if (regex.test(data.type) === false) {
+                flag.push(false)
+                error.type = `APP: The phone number is invalid.`
+            }
+            const pn = parsePhoneNumber(data.contact_data, { regionCode: props })
+            if (pn?.valid == false) {
+                flag.push(false)
+                error.contact_data = `APP: The phone number is invalid.`
+            }
+        }
+    }
+
+    if (data.contact_type !== "" && data.contact_type !== undefined) {
+        if (data.contact_type === "tdd number") {
+            if ((!data?.contact_data?.match(/^([1-9]+[0-9]*)$/))) {
+                flag.push(false)
+                error.contact_data = "APP: The tdd number is invalid."
+            }
+        }
+    }
+    if (data.contact_type !== "" && data.contact_type !== undefined) {
+        if (data.contact_type === "toll free number") {
+            if ((!data?.contact_data?.match(/^([1-9]+[0-9]*)$/))) {
+                flag.push(false)
+                error.contact_data = "APP: The toll free number is invalid."
+            }
+        }
+    }
+
+
+    for (let value in flag) {
+
+        if (flag[value] === false) {
+            final_flag = false;
+            break;
+        }
+
+    }
+
+    return final_flag === true ? true : error;
+}
+export default validateContact
 
--- components/validation/contact/contactedit.js
+++ components/validation/contact/contactedit.js
@@ -1,72 +1,75 @@
 import { parsePhoneNumber } from "awesome-phonenumber";
-const validateContactEdit = (data,props) =>{
-    
-    var error={};
-     var flag=[]
-     var final_flag=true;
-    
-    if(data.name==="" || data.name===undefined)
-    {
+const validateContactEdit = (data, props) => {
+
+    var error = {};
+    var flag = []
+    var final_flag = true;
+
+    if (data.name === "" || data.name === undefined) {
         flag.push(false)
-        error.name="APP: The contact type is required"
+        error.name = "APP: The contact type is required"
     }
 
-    if(data.type==="" || data.type===undefined)
-    {
+    if (data.type === "" || data.type === undefined) {
         flag.push(false)
-        error.type="APP: The contact data is required."
+        error.type = "APP: The contact data is required."
     }
 
-   
-    if(data.name === "Email"){  
-    if((!data.type?.match(/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/))){
-        flag.push(false)
-        error.type="APP: The email is invalid."
-    }
-}
-if(data.name === "website"){  
-    if((!data.type?.match(/^(?:http(s)?:\/\/)?[\w.-]+(?:\.[\w\.-]+)+[\w\-\._~:/?#[\]@!\$&'\(\)\*\+,;=.]+$/))){
-        flag.push(false)
-        error.contact_data="APP: The website is invalid."
-    }
-}
-   
-    if(data.name !== "" && data.name !== undefined){
-        if(data.name === "Tdd Number"){  
-        if((!data.type?.match(/^([1-9]+[0-9]*)$/))){
+
+    if (data.name === "Email") {
+        if ((!data.type?.match(/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/))) {
             flag.push(false)
-            error.type="APP: The tdd number is invalid."
+            error.type = "APP: The email is invalid."
         }
     }
+    if (data.name === "website") {
+        if ((!data.type?.match(/^(?:http(s)?:\/\/)?[\w.-]+(?:\.[\w\.-]+)+[\w\-\._~:/?#[\]@!\$&'\(\)\*\+,;=.]+$/))) {
+            flag.push(false)
+            error.contact_data = "APP: The website is invalid."
         }
-     if(data.name !== "" && data.name !== undefined){
-            if(data.name === "Toll Free Number"){  
-            if((!data.type?.match(/^([1-9]+[0-9]*)$/))){
+    }
+
+    if (data.name !== "" && data.name !== undefined) {
+        if (data.name === "Tdd Number") {
+            if ((!data.type?.match(/^([1-9]+[0-9]*)$/))) {
                 flag.push(false)
-                error.type="APP: The toll free number is invalid."
+                error.type = "APP: The tdd number is invalid."
             }
         }
-     }
-     if(data.name !== "" && data.name !== undefined){
-        if(data.name === "Phone"){  
-        const pn = parsePhoneNumber(data.type, {regionCode:props})
-        if (pn?.valid == false) {
-            flag.push(false)
-            error.type= `APP: The phone number is invalid.`
-          }
     }
+    if (data.name !== "" && data.name !== undefined) {
+        if (data.name === "Toll Free Number") {
+            if ((!data.type?.match(/^([1-9]+[0-9]*)$/))) {
+                flag.push(false)
+                error.type = "APP: The toll free number is invalid."
+            }
         }
-    for (let value in flag) {
-     
-        if(flag[value]=== false)
-         {
-           final_flag = false;
-           break;
-         }
-         
-      } 
-
-      return final_flag===true ? true : error;
     }
-export default  validateContactEdit
+    if (data.name !== "" && data.name !== undefined) {
+        if (data.name === "Phone") {
+            // regex to check string may have + at start , may have - after 2 digits and must have only numbers
+            const regex = /^(\+)?\d{2}(-)?\d+$/;
+            if (regex.test(data.type) === false) {
+                flag.push(false)
+                error.type = `APP: The phone number is invalid.`
+            }
+            const pn = parsePhoneNumber(data.type, { regionCode: props })
+            if (pn?.valid == false) {
+                flag.push(false)
+                error.type = `APP: The phone number is invalid.`
+            }
+        }
+    }
+    for (let value in flag) {
+
+        if (flag[value] === false) {
+            final_flag = false;
+            break;
+        }
+
+    }
+
+    return final_flag === true ? true : error;
+}
+export default validateContactEdit
 
